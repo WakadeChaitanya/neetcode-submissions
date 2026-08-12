e8ab01f622c543aaab4006539a4d9087054dbc63
WakadeChaitanya <wakadechaitanya@gmail.com>
2026-08-12 23:27:27 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self,s,t):
+        if len(s)!=len(t):
+            return False
+        return Counter(s)==Counter(t)
+l=Solution()
+print(l.isAnagram('racecar','carrace'))
+        
